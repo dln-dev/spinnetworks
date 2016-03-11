16aaa1c34e82137078b67bf1cfe082410c1f4c6b
dln-dev <dln-dev@posteo.de>
2016-03-11 16:16:22 +0100

    Add spinNetworks.py
    Puts it all together. Import this for usage.

---
+++ spinNetworks.py
@@ -0,0 +1,6 @@
+#import models.models as models
+#import views.views as views
+#import controls.controls as controls
+from models.models import *
+from views.views import *
+from controls.controls import *
