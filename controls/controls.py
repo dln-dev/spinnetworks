93fae1cc9f94552254f390384a82444624b97681
dln-dev <dln-dev@posteo.de>
2016-03-16 14:21:43 +0100

    Add switchEdges() generating function

--- controls/controls.py
+++ controls/controls.py
@@ -5,3 +5,10 @@ def chainEdges(qubits,periodic=False):
     if periodic:
         edges += [(vertices[qubits-1],vertices[0])]
     return edges
+
+
+# generates simple star network where node 1 is the central spin
+def switchEdges(qubits):
+    vertices = [ 2**j for j in range(0,qubits) ]
+    edges = [ (1,vertices[i]) for i in range(1,qubits) ]
+    return edges
