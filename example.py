355324b3e2bf10f9d63cdec7b978f613cca2146f
dln-dev <dln-dev@posteo.de>
2016-03-16 14:20:24 +0100

    Fix example.py

--- example.py
+++ example.py
@@ -1,7 +1,13 @@
 import spinNetworks as sn
 
 graph = sn.spinNetwork(4,sn.chainEdges(4))
-graph.getVertices(2)
-graph.getEdges(2)
+print(graph.getVertices(4))
+print(graph.getEdges(2))
 
 sn.mathematicaPrint(graph)
+
+switch = sn.spinNetwork(4,sn.switchEdges(4))
+print(switch.getVertices(2))
+print(switch.getEdges(2))
+
+sn.mathematicaPrint(switch)
