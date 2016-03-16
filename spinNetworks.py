b4b62fec1ab2f5d8afa0ef1614f4ae132df3d8b2
dln-dev <dln-dev@posteo.de>
2016-03-16 14:20:58 +0100

    Update spinNetworks.py

--- spinNetworks.py
+++ spinNetworks.py
@@ -1,6 +1,3 @@
-#import models.models as models
-#import views.views as views
-#import controls.controls as controls
 from models.models import *
 from views.views import *
 from controls.controls import *
